7c46c2522ae2e0c49cee52ae6008cac2e4297724
Jengyoung <young_pallete@naver.com>
2021-07-06 11:01:38 +0900

    ESM 표준 방식으로 모듈 import, export하기

--- index.js
+++ index.js
@@ -4,7 +4,15 @@
  * 3. 공식을 통해 결과 얻기
  * */ 
 
-const { getCircleArea } = require('./mathUtil');
+// const { getCircleArea } = require('./mathUtil');
+// import { getCircleArea } from './mathUtil';
 
-const result = getCircleArea(2);
+// const result = getCircleArea(2);
+// console.log(result);
+
+// export default로 내보냈을 경우 객체 이름으로 접근해야 함.
+import mathUtil from './mathUtil';
+
+
+const result = mathUtil.getCircleArea(2);
 console.log(result);
--- mathUtil.js
+++ mathUtil.js
@@ -9,5 +9,21 @@ const getCircleArea = r => r * r * PI;
 
 
 //각각 키에 값을 할당하여 내보내기
-exports.PI = PI;
-exports.getCircleArea = getCircleArea;
+// exports.PI = PI;
+// exports.getCircleArea = getCircleArea;
+
+
+/*
+    npm install esm
+    node -r esm [파일명]
+    * commonJS와 esm 표준 방식으로 둘다 읽을 수 있다.
+*/ 
+// export {
+//     PI,
+//     getCircleArea,
+// }
+
+export default {
+    PI,
+    getCircleArea
+}
