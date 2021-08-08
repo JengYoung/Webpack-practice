3393daae40605c97f0d639a621bb84c4f232053d
Jengyoung <young_pallete@naver.com>
2021-08-08 23:07:21 +0900

    feat: 템플릿 엔진 설정, hash 설정

--- webpack-loader/webpack.config.js
+++ webpack-loader/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -25,14 +25,21 @@ module.exports = {
                         }
                     }
                 ]
+            }, {
+                test: /\.hbs$/,
+                use: ['handlebars-loader']
             }
         ]
     },
     plugins: [
         // 외부 저장소에서 관리되어지는 플러그인은, 로더와 마찬가지로 README.md에서 자세히 살펴봐야 함.
         new HtmlWebpackPlugin({
+            title: 'webpack',
             // 자동으로 생성되는 html이 특정 파일을 기준으로 만들어지게끔 파일을 지정해주는 역할.
-            template: './template.html'
+            template: './template.hbs',
+            meta: {
+                viewport: "width-device-width, initial-scale=1.0"
+            }
         })
     ],
     mode: 'none'
